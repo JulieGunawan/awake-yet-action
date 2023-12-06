b536aae06bf7ea00bfdc2d945a4bc785758fe33c
JulieGunawan <48563515+JulieGunawan@users.noreply.github.com>
2023-12-06 19:32:47 +0000

    create more var

--- index.js
+++ index.js
@@ -22,9 +22,11 @@ async function run(){
     const {context} = github;
 
     console.log(context.payload);
+    const pullRequestNumber = context.payload.pull_request.number;
+    const prTitle = context.payload.pull_request.title;
     const octokit = new github.GitHub(githubToken);
     const message = "hello world\n";
-    
+
     octokit.issues.createComment({
       ...context.repo,
       issue_number: pullRequestNumber,
